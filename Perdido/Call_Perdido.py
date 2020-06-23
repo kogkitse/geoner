882f36e6255ea1c8ba97d2c8774a12f3603833ca
Eleni Kogkitsidou <me@kogkitse.org>
2020-06-23 02:12:34 -1000

    Add Perdido

---
+++ Perdido/Call_Perdido.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf8
+# Project : "Cité des dames : créatrices dans la cité"
+
+import glob, os, re, sys, json, requests, time
+import requests_cache
+from nltk.tokenize import sent_tokenize
+from os import path
+
+requests_cache.install_cache('perdido_cache')
+
+# Please replace the API key below by your own (get it from the dashboard of Dandelion)
+apiKey = "faHfUX2B4L"
+
+# urlBegin_1 ="http://erig.univ-pau.fr/PERDIDO/api/nerc/txt_xml/?lang=French&api_key="+apiKey+"&content="
+# urlBegin_2 ="http://erig.univ-pau.fr/PERDIDO/api/toponyms/txt_json/?lang=French&api_key="+apiKey+"&content="
+urlBegin_3 = "http://erig.univ-pau.fr/PERDIDO/api/geoparsing/?lang=French&api_key="+apiKey+"&content="
+
+#text = ["Je sortis par la porte qui ouvre sur la Seine, et ne repris haleine que dans la rue, par laquelle on va du Petit Pont à la place Saint-Michel, et qu’on appelle, je crois, la rue de la Calandre.","De là je gagnai la place Saint-Germain, et j’arrivai au puits encore toute émue.","Quelle belle cérémonie ! Elle eut lieu le 25 décembre 496, dans l’église de Reims.","Mais une fois, dans un combat qu’il livrait aux Allemands, à Tolbiac, se voyant près de succomber, il invoqua hautement le Dieu de madame Clotilde","Mais en arrivant près de Châlons, on apprit que la Saône avait débordé à plusieurs endroits, et que la navigation était difficile, surtout en revenant de Lyon."]
+file = sys.argv[1]
+
+
+# Create the result file to write inside
+# outputXML = open("D:\CiteDames\Perdido/results_url_XML.txt", "w", encoding="utf-8")
+# outputJSON = open("D:\CiteDames\Perdido/results_url_JSON.txt", "w", encoding="utf-8")
+
+basename = os.path.basename(file) 
+directory = os.path.dirname(sys.argv[0])
+base_prefix = os.path.splitext(basename)[0]
+base_rename_extention = "{}".format(base_prefix + '.xml')
+output_path = path.join(directory+"\output_perdido", "Perdido_" + base_rename_extention)
+output = open(output_path, "w", encoding="utf-8")
+
+#i = 0
+
+# Treat the first sentence of the text and calls itself on the remaining sentences after waiting 2 seconds
+def analyseRemainingSentences(text):
+   # loop over senteces
+   for i, sentence in enumerate(text):
+      # check if sentence isn't empty
+      if len(text) > 0:
+         # analyze the sentence using the Perdido API
+         # try to call the API every 2 seconds until get a 200 response
+         while True:
+            print("\n\nAnalyzing « " + sentence + " »"+" No: " + str(i+1))
+            # Analyze it with the Perdido API
+            # response_1 = requests.get(urlBegin_1 + sentence)
+            # response_2 = requests.get(urlBegin_2 + sentence)
+            response_3 = requests.get(urlBegin_3 + sentence)
+            if response_3.status_code == 200:
+               break
+            # wait for 2 seconds before analyzing the next sentence
+            time.sleep(2)
+
+         # save the full result
+         if response_3.text:
+            # outputXML.writelines(response_1.text)
+            # outputJSON.writelines(response_2.text)
+            output.writelines(response_3.text)
+        
+def open_doc(file): 
+    with open(file, encoding='utf-8') as file:
+        read_line = file.read()
+        return read_line
+
+# Open and read a file line by line
+with open(file, 'r', encoding="utf8") as file:
+   text = file.read()
+   match_punct =r"…"
+   subst_punct = ""
+   text = re.sub(match_punct, subst_punct, text, 0, re.MULTILINE)
+
+#  Nltk tokenizer split text into sentences
+   text = (sent_tokenize(text))
+
+# Start analyzing the text
+analyseRemainingSentences(text)
+# outputXML.close
+# outputJSON.close
+output.close
+
+# Clean xml to get txt with placeName tags
+
+
+
---
+++ Perdido/perdido_hypo_postprocessing.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf8
+# Project : "Cité des dames : créatrices dans la cité"
+
+from pathlib import Path
+import re
+from scrapy import Selector
+from os import path
+import os
+import sys
+
+file = sys.argv[1]
+
+
+
+# Read file
+def open_doc(filename): 
+    with open(filename, 'r', encoding='utf-8') as file:
+        read_line = file.read()
+        return read_line
+
+def write_doc(filename, text_write):
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text_write)
+
+def tag_LOC(output_hypo):
+    # Match locations tags
+    match_LOC = r"<[^>]*>([^>]*)</w>"
+    # Match punct 
+    match_punct = r"\n|\)|\(|\[|\]|»|«|…"
+    # Match '?,.,!'
+    match_split = r"(\.\s*|\!\s*|\?\s*)(\w|<)"
+
+    # Substitute Location tags with <placeName>
+    subst_LOC = "<placeName>\\1</placeName>"
+    # Substitute punct
+    subst_punct = ""
+    # Substitute punctuation in order to split sentences line by line
+    subst_punct_split = "\\1\\n\\2"
+    
+    result_LOC = re.sub(match_LOC, subst_LOC, output_hypo, 0, re.MULTILINE)
+    result_punct = re.sub(match_punct, subst_punct, result_LOC, 0, re.MULTILINE)
+    result_hypo_split = re.sub(match_split, subst_punct_split, result_punct, 0, re.MULTILINE)
+    return(result_hypo_split)
+
+
+
+basename = os.path.basename(file) 
+directory = os.path.dirname(sys.argv[0])
+base_prefix = os.path.splitext(basename)[0]
+base_rename_extention = "{}".format(base_prefix + '.xml')
+output_path = path.join(directory+"\output_perdido", "Perdido_" + base_rename_extention)
+
+# Scrapy lib will call xpath which match every text balise exept //w with @type="NPr"
+output = Selector(text = open_doc(output_path))
+requete= output.xpath('//w[not(@type="NPr")]/text() | //name[@type="place"]/w | //name[@type="unknown"]/w/text()| //name[@type="person"]/w/text()| //p').extract()
+separator = ' '
+clean_output = separator.join(requete)
+clean_path = path.join(directory+"\output_perdido", "Perdido_clean_" + base_rename_extention)
+write_doc(clean_path, clean_output)
+
+base_rename_extention = "{}".format(base_prefix + '.txt')
+hypo_output_path = path.join(directory+"\output_perdido", "hypo_perdido_" + base_rename_extention)
+
+write_doc(hypo_output_path, tag_LOC(clean_output))
+# with open(hypo_output_path, 'w', encoding='utf-8') as hypo_output:
+#     hypo_output.write(tag_LOC(clean_output))

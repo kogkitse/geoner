26185dc61d23f1de7927438e7b4681f4fadc49ed
Eleni Kogkitsidou <me@kogkitse.org>
2020-07-07 11:19:44 -1000

    add normalisation output

--- CasEN/casen.py
+++ CasEN/casen.py
@@ -1,4 +1,7 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# Project Cité des Dames 
+#  Author : kogkitse
+
 import sys, os, re, shutil 
 from os import path
 
@@ -32,48 +35,53 @@ file_text_path = path.join(private_dir+'\\Corpus\\'+basename)
 with open(file_text_path, "w", encoding="utf-8") as file_text_path: 
     file_text_path.write(file_endline)
 
-os.system('mkdir "'+input_corpus+base_prefix+'_snt"')
+# os.system('mkdir "'+input_corpus+base_prefix+'_snt"')
 
 # Normalise input
 
-os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Normalize "'+private_dir+'\\corpus\\'+base_prefix+'.txt" "-r'+unitexFrenchFolder+'Norm.txt" "--output_offsets='+private_dir+'\\corpus\\'+base_prefix+'_snt\\normalize.out.offsets" -qutf8-no-bom"')
+# os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Normalize "'+private_dir+'\\corpus\\'+base_prefix+'.txt" "-r'+unitexFrenchFolder+'Norm.txt" "--output_offsets='+private_dir+'\\corpus\\'+base_prefix+'_snt\\normalize.out.offsets" -qutf8-no-bom"')
 
 # Tokenise input
 
-os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Tokenize "'+unitexFrenchFolder+'corpus\\'+base_prefix+'.snt" "-a'+unitexFrenchFolder+'Alphabet.txt" "--input_offsets='+unitexFrenchFolder+'corpus\\'+base_prefix+'_snt\\normalize.out.offsets" "--output_offsets='+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\tokenize.out.offsets" -qutf8-no-bom"')
+# os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Tokenize "'+unitexFrenchFolder+'corpus\\'+base_prefix+'.snt" "-a'+unitexFrenchFolder+'Alphabet.txt" "--input_offsets='+unitexFrenchFolder+'corpus\\'+base_prefix+'_snt\\normalize.out.offsets" "--output_offsets='+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\tokenize.out.offsets" -qutf8-no-bom"')
 
 
 # Apply lexical resources 
 
-os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Dico "-t'+unitexFrenchFolder+'Corpus\\'+base_prefix+'.snt" "-a'+unitexFrenchFolder+'Alphabet.txt" "-m'+dela_system+'\\profession.bin" "'+dela_system+'\\profession.bin" "'+dela_system+'\\Dela_fr.bin" "'+unitexFrenchFolder+'Dela\\CasEN_Ambiguites-.bin" "'+unitexFrenchFolder+'Dela\\CasEN_Dico.bin" "'+unitexFrenchFolder+'Dela\\Prolex-Unitex-BestOf_2_2_fra.bin" -qutf8-no-bom"')
+# os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Dico "-t'+unitexFrenchFolder+'Corpus\\'+base_prefix+'.snt" "-a'+unitexFrenchFolder+'Alphabet.txt" "-m'+dela_system+'\\profession.bin" "'+dela_system+'\\profession.bin" "'+dela_system+'\\Dela_fr.bin" "'+unitexFrenchFolder+'Dela\\CasEN_Ambiguites-.bin" "'+unitexFrenchFolder+'Dela\\CasEN_Dico.bin" "'+unitexFrenchFolder+'Dela\\Prolex-Unitex-BestOf_2_2_fra.bin" -qutf8-no-bom"')
 
 
 # Run cassys 
 
-os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Cassys  "-a'+unitexFrenchFolder+'Alphabet.txt" "-t'+unitexFrenchFolder+'Corpus\\'+base_prefix+'.snt" "-l'+unitexFrenchFolder+'Cassys\\CasEN_analyse_GEO.csc" "-w'+dela_system+'\\Dela_fr.bin" -v -r'+unitexFrenchFolder+'Graphs\\ "--input_offsets='+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\normalize.out.offsets" -qutf8-no-bom"')
+# os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Cassys  "-a'+unitexFrenchFolder+'Alphabet.txt" "-t'+unitexFrenchFolder+'Corpus\\'+base_prefix+'.snt" "-l'+unitexFrenchFolder+'Cassys\\CasEN_analyse_GEO.csc" "-w'+dela_system+'\\Dela_fr.bin" -v -r'+unitexFrenchFolder+'Graphs\\ "--input_offsets='+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\normalize.out.offsets" -qutf8-no-bom"')
 
 
 # Extract output
 
-os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Concord "'+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\concord.ind" "-m'+unitexFrenchFolder+'Corpus\\'+base_prefix+'-raw.txt" -qutf8-no-bom"')
+# os.system('CMD /c ""'+unitex_directory+'UnitexToolLogger.exe" Concord "'+unitexFrenchFolder+'Corpus\\'+base_prefix+'_snt\\concord.ind" "-m'+unitexFrenchFolder+'Corpus\\'+base_prefix+'-raw.txt" -qutf8-no-bom"')
 
 
-# Post processing output 
+# Post processing clean output 
 
 output = unitexFrenchFolder+'Corpus\\'+base_prefix+'-raw.txt'
 output_file = open_doc(output)
 
+regex_unitex_tags = r"{S} | {S}|{S}"
 
+regex_tags = r"(\{|(\\\{))+([^,\\]*)(\\,\\.|,.)([^+|\\]*)[^\s]*"
+regex_geogName = r"(<|</)geogName>"
 
+subst_unitex_tags = ""
+subst_tags = "<\\5>\\3</\\5>"
+subst_geogName = "\\1placeName>"
 
-
-
+result_unitex_tags= re.sub(regex_unitex_tags, subst_unitex_tags, output_file, 0, re.MULTILINE)
+result_tags = re.sub(regex_tags, subst_tags, result_unitex_tags, 0, re.MULTILINE)
+result_geogName = re.sub(regex_geogName, subst_geogName, result_tags, 0, re.MULTILINE)
 
 
 # write output to output_casen directory
 base_rename_extention = "{}".format('hypo_casEN_' + base_prefix + '.txt')
 outuput_path = (os.path.dirname(sys.argv[0])+"\output_casen\\"+ base_rename_extention)
-
-
-# Copie/paste file 
-# shutil.copy2(unitexFrenchFolder+'Corpus\\'+base_prefix+'-raw.txt', outuput_path)
+with open(outuput_path, "w", encoding="utf-8") as hypothesis: 
+    hypothesis.write(result_geogName)

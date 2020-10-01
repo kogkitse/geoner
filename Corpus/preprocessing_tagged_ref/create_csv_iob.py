6b72af7be83a61031c265bf09e4ec07d387d555d
Eleni Kogkitsidou <me@kogkitse.org>
2020-09-30 23:34:42 -1000

    Update Corpus/preprocessing_tagged_ref/create_csv_iob.py

--- Corpus/preprocessing_tagged_ref/create_csv_iob.py
+++ Corpus/preprocessing_tagged_ref/create_csv_iob.py
@@ -9,21 +9,23 @@ import sys, os, re, shutil
 import pandas as pd  
 from os import path
 
-# Enter coprus file
+
+# Enter coprus file, for example : 
+# ./create_csv_iob.py ../Margot_wikisource_20200825\MargueriteDeValois-original.txt 
 corpus_dir = os.path.dirname(sys.argv[1])
 dir_corpusname = os.path.basename(corpus_dir)
 basename = os.path.basename(sys.argv[1]) 
 basename_prefix = os.path.splitext(basename)[0]
 
-casen_file = './CasEN/output_casen/ouput_' + dir_corpusname +'\hypo_casEN_' + basename_prefix +'_IOB.txt'
-print(casen_file)
-spacy_file = './Spacy/output_spacy/ouput_' + dir_corpusname +'\\hypo_spaCy_' + basename_prefix + '_IOB.txt'
-corenlp_file = './CoreNLP/output_corenlp/ouput_' + dir_corpusname +'\\hypo_CoreNLP_' + basename_prefix + '_IOB.txt'
-sem_file = './SEM/output_sem/ouput_' + dir_corpusname +'\\hypo_SEM_' + basename_prefix + '_IOB.txt'
-perdido_file = './Perdido/output_perdido/ouput_' + dir_corpusname +'\hypo_Perdido_' + basename_prefix + '_IOB.txt'
+casen_file = './CasEN/output_casen/ouput_' + dir_corpusname +'\hypo_casEN_' + basename_prefix +'_IOB.csv'
+spacy_file = './Spacy/output_spacy/ouput_' + dir_corpusname +'\\hypo_spaCy_' + basename_prefix + '_IOB.csv'
+corenlp_file = './CoreNLP/output_corenlp/ouput_' + dir_corpusname +'\\hypo_CoreNLP_' + basename_prefix + '_IOB.csv'
+sem_file = './SEM/output_sem/ouput_' + dir_corpusname +'\\hypo_SEM_' + basename_prefix + '_IOB.csv'
+perdido_file = './Perdido/output_perdido/ouput_' + dir_corpusname +'\hypo_Perdido_' + basename_prefix + '_IOB.csv'
 
 
 words = [] 
+words_sem = []
 tags_spacy = []   
 tags_sem = []   
 tags_casen = []   
@@ -43,14 +45,17 @@ def tags_in_list(tags, matches_tags):
 
 #  match words of csv file
 regex_words = r"^\w+|^\W"
-matches_words = re.finditer(regex_words, open_doc(spacy_file), re.MULTILINE)
-
+matches_words = re.finditer(regex_words, open_doc(casen_file), re.MULTILINE)
 # add words in a list
 for match_word in matches_words:
     words.append(match_word.group())
 
+output_path = path.join("./evaluation/files/"+ basename_prefix +'/'+ basename_prefix + "_words.csv")
+df = pd.DataFrame(list(zip(words)))
+df.to_csv(output_path, encoding='utf-8', index=False)
+
 #  match tags of csv file
-regex_tags = r"O|B-LOC|I-LOC"
+regex_tags = r"O$|B-LOC|I-LOC"
 matches_tags_spacy = re.finditer(regex_tags, open_doc(spacy_file), re.MULTILINE)
 matches_tags_sem = re.finditer(regex_tags, open_doc(sem_file), re.MULTILINE)
 matches_tags_casen = re.finditer(regex_tags, open_doc(casen_file), re.MULTILINE)
@@ -63,9 +68,15 @@ spacy_tag = tags_in_list(tags_spacy, matches_tags_spacy)
 perdido_tag = tags_in_list(tags_perdido, matches_tags_perdido)
 corenlp_tag = tags_in_list(tags_corenlp, matches_tags_corenlp)
 
+output_path = path.join("./evaluation/files/"+ basename_prefix +'/'+ basename_prefix + "_tags.csv")
+df = pd.DataFrame(list(zip(spacy_tag)))
+df.to_csv(output_path, encoding='utf-8', index=False)
 
-# create a dataframe with words and tags
+# create a dataframe with words and tags from each tool's output
 df = pd.DataFrame(list(zip(words, spacy_tag, casen_tag, sem_tag, perdido_tag, corenlp_tag)), 
 columns =['Word', 'Spacy_tag', 'CasEN_tag', 'SEM_tag', 'Perdido_tag', 'CoreNLP_tag'])
 
-
+# save output in IOB format from dataframe to csv
+output_path = path.join("./evaluation/files/"+ basename_prefix +'/'+ basename_prefix + "_IOB.csv")
+df.to_csv(output_path, encoding='utf-8', index=False)
+print("Output created at : " + "./evaluation/files/"+ basename_prefix )
